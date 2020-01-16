4e2c4496e66ad3c19edce8fab1eb072502d17ed8
KRM-24 <36944142+KRM-24@users.noreply.github.com>
2020-01-15 21:15:29 -0600

    Udemy 3_40 thru 3_41
    put CommentDetail inside of ApprovalCard via props.children

--- index.js
+++ index.js
@@ -7,25 +7,36 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui containt comments">
-            <ApprovalCard />
-            <CommentDetail 
-                author="Sam" 
-                timeAgo="Today at 4:45PM" 
-                content="Nice blog post!" 
-                avatar={faker.image.avatar()} 
-            />
-            <CommentDetail 
-                author="Alex" 
-                timeAgo="Today at 2:00AM" 
-                content="Nice blog!" 
-                avatar={faker.image.avatar()} 
-            />
-            <CommentDetail 
-                author="Jane" 
-                timeAgo="Yesterday at 5:00PM" 
-                content="Nice post!" 
-                avatar={faker.image.avatar()} 
-            />
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Today at 4:45PM" 
+                    content="Nice blog post!" 
+                    avatar={faker.image.avatar()} 
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Alex" 
+                    timeAgo="Today at 2:00AM" 
+                    content="Nice blog!" 
+                    avatar={faker.image.avatar()} 
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Jane" 
+                    timeAgo="Yesterday at 5:00PM" 
+                    content="Nice post!" 
+                    avatar={faker.image.avatar()} 
+                />
+            </ApprovalCard>
         </div>
     );
 };
